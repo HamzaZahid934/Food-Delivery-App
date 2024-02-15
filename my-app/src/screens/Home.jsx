c814b6c277dadfc59a23a572e484ccc0b38dc7e6
HamzaZahid934 <146257691+HamzaZahid934@users.noreply.github.com>
2024-02-15 16:52:00 +0500

    first commit

---
+++ my-app/src/screens/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                body
+            </div>
+            <div>
+                <Footer/>
+            </div>
+
+
+        </div>
+    )
+}
