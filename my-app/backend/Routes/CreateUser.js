5d32409cdb978ec3061f7f956c0e0f40ec695cff
HamzaZahid934 <146257691+HamzaZahid934@users.noreply.github.com>
2024-02-18 20:14:21 +0500

    .

--- my-app/backend/Routes/CreateUser.js
+++ my-app/backend/Routes/CreateUser.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const user = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post("/createuser",async (req,resp)=>{
+router.post("/createuser",[
+    // Validation middleware
+body('name').notEmpty().withMessage('Name is required'),
+body('password').notEmpty().withMessage('Password is required').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format')],
+async (req,resp)=>{
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return resp.status(400).json({ success: false, errors: errors.array() });
+    }
  try {
     await user.create({
 
